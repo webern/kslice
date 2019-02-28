28c486075584ef5e81fa474e00c6b9263550ef06
Matthew James Briggs <matthew.james.briggs@gmail.com>
2019-02-28 14:19:34 -0800

    cleanup

--- lib/include/kscore/Slice.h
+++ lib/include/kscore/Slice.h
@@ -3,85 +3,19 @@
 #include <opencv2/core/mat.hpp>
 
 #include <cstdint>
-#include <vector>
-#include <set>
 #include <iostream>
+#include <set>
+#include <vector>
 
 namespace kslice {
 
     struct Slice {
-    public:
+      public:
         uint64_t hash;
         double seconds;
         cv::Mat data;
 
-        void toStream(std::ostream& os) const;
+        void toStream( std::ostream &os ) const;
     };
 
-    // takes N equally-spaced samples from the slice set provided that slices.size() >= N
-    inline std::vector<Slice> sample(const std::set<Slice>& slices, int inNumSamples) {
-        if( inNumSamples <= 0 ) {
-            return std::vector<Slice>{};
-        }
-
-        std::vector<Slice> result;
-        const auto div = slices.size() / static_cast<size_t>( inNumSamples );
-        const auto stride = div > 0 ? div : 1;
-        size_t current = 0;
-
-        for ( const auto& slice : slices )
-        {
-            if( current % stride == 0 )
-            {
-                result.emplace_back( slice );
-            }
-            current++;
-
-            if( result.size() >= inNumSamples )
-            {
-                break;
-            }
-        }
-
-        const auto less = [&]( Slice& a, Slice& b ) {
-            return a.seconds < b.seconds;
-        };
-
-        std::sort(std::begin(result), std::end(result), less);
-        return result;
-    }
-
-
-    inline bool operator<(const Slice &a, const Slice &b) {
-        if (a.hash < b.hash) {
-            return true;
-        }
-
-        if (a.hash > b.hash) {
-            return false;
-        }
-
-        return a.seconds < b.seconds;
-    }
-
-    inline bool operator>(const Slice &a, const Slice &b) {
-        return b < a;
-    }
-
-    inline bool operator==(const Slice &a, const Slice &b) {
-        return !(a < b) && !(b < a);
-    }
-
-    inline bool operator!=(const Slice &a, const Slice &b) {
-        return !(a == b);
-    }
-
-    inline bool operator<=(const Slice &a, const Slice &b) {
-        return (a < b) || (a == b);
-    }
-
-    inline bool operator>=(const Slice &a, const Slice &b) {
-        return (a > b) || (a == b);
-    }
-
-}
+} // namespace kslice
